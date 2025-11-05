9833da7c4d6a03efce9ab2811b418ee21b843205
Dasha <dasha2312s3@gmail.com>
2025-11-05 13:28:29 +0200

    added components

--- test-assessment/src/App.tsx
+++ test-assessment/src/App.tsx
@@ -2,14 +2,16 @@
 import { menuItems } from './components/SidebarMenu/data'
 import SidebarMenu from './components/SidebarMenu/SidebarMenu'
 import Input from './UI/Input/InputBase'
+import Toast from './UI/Toast/Toast'
 
 function App() {
 
   return (
-    <>
+    <div className='container'>
       <Input typeInput='password' />
       <SidebarMenu menuItems={menuItems} />
-    </>
+      <Toast message="test message here" toastType="success" />
+    </div>
   )
 }
 
